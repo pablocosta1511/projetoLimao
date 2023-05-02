2a7de919f275e53715babea35fd050a421bbcdaf
Pablo <pablocostap15117@gmail.com>
2023-05-02 17:01:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetolimao/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.projetolimao
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
+import com.example.projetolimao.ui.theme.ProjetoLimaoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjetoLimaoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    appLimonada()
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun appLimonada() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.limoeiro)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.lemon_tree),
+            contentDescription = null,
+            modifier = Modifier
+                .size(400.dp)
+                .border(
+                    BorderStroke(2.dp, Color.Cyan),
+                    RoundedCornerShape(5.dp)
+                )
+        )
+
+    }
+}
+
